9d669f153bb773c63f60cb683f01cd23626d14e7
eapo <e@eaposztrof.com>
2022-09-27 18:25:23 +0200

    "Invite only" more info in HTML

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -15,12 +15,17 @@
               The Altruist Paltform is closed and one can only join by an invitation 
               from at least three existing users (who gained invitations in the system),
               and therefore self-curating to keep the quality of content in general. 
-              That’s not a very democratic statement, but resources plus democracy equals ![_Trolling_](https://i.imgur.com/IJlEc0b.png)
-              and the lack of _Trust_, which plays a key role in _Altruism_.
+              That’s not a very democratic statement, but resources plus democracy equals <i><img class="placeholder" src="https://i.imgur.com/IJlEc0b.png" alt="Trolling" />
+              and the lack of <i>Trust</i>, which plays a key role in </i>Altruism</i><sup id="cite_ref-1" class="reference"><a href="#fn1" id="#fnref1" name="#fnref1" class="keychainify-checked">[2]</a></sup>.
             </p>
             <a href="#!" class="card-link TODO"  @click="registerActive = !registerActive" title="Be one ofthe 1st to join the beta!">Book Now!</a>
             <a href="#!" class="card-link"  @click="registerActive = !registerActive" title="Sign up here if you are invited.">Sign up with Invited email address</a>
           </div>
+          <ol class="footnotes-list card-footer">
+            <li id="fn1" class="footnote-item">
+              <a href="https://en.wikipedia.org/wiki/Altruism" target="_blank" rel="noopener" class="keychainify-checked">Altruism - Wikipedia</a>, <a href="https://ethicsunwrapped.utexas.edu/glossary/altruism" target="_blank" rel="noopener" class="keychainify-checked">Altruism - Ethics Unwrapped</a> <a href="#fnref1" class="footnote-backref keychainify-checked" smoothhashscroll="">↩︎</a></p>
+            </li>
+          </ol>
         </div>
       </div>
       <div v-if="registerActive" class="container-sm">
