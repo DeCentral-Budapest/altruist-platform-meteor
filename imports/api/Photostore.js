f07a916a3cbd13ff83101b0550c9c12fbaffc309
kmildi <keleti.ildi@gmail.com>
2022-09-28 21:47:30 +0200

    Replace store permissions

--- imports/api/Photostore.js
+++ imports/api/Photostore.js
@@ -1,20 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
 import { GridFSStore } from 'meteor/jalik:ufs-gridfs';
 import { UploadFS } from 'meteor/jalik:ufs';
 import { Photos } from '../api/collections/Photos.js';
 
-export const PhotoStore = new GridFSStore({
-  collection: Photos,
-  name: 'photoStore',
-  chunkSize: 1024 * 255,
-  filter: new UploadFS.Filter({
-    contentTypes: ['image/*'],
-    maxSize: 2000000, // 2MB,
-  }),
-});
-
-PhotoStore.setPermissions(new UploadFS.StorePermissions({
+const permissions = new UploadFS.StorePermissions({
   insert(userId, doc) {
     return !!userId;
   },
@@ -24,4 +12,15 @@ PhotoStore.setPermissions(new UploadFS.StorePermissions({
   remove(userId, doc) {
     return !!userId;
   },
-}));
+})
+
+export const PhotoStore = new GridFSStore({
+  collection: Photos,
+  name: 'photoStore',
+  chunkSize: 1024 * 255,
+  filter: new UploadFS.Filter({
+    contentTypes: ['image/*'],
+    maxSize: 2000000, // 2MB,
+  }),
+  permissions,
+});
