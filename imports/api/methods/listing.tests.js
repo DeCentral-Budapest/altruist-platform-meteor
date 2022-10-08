e35da5b18d8c477a705b1c1d834727e7752d12be
kmildi <keleti.ildi@gmail.com>
2022-10-08 09:05:18 +0200

    Updating listings tests

--- imports/api/methods/listing.tests.js
+++ imports/api/methods/listing.tests.js
@@ -5,15 +5,21 @@ import '../methods/listings.js';
 
 if (Meteor.isServer) {
   describe('method: listings', function () {
+    let testUserId;
+    before(function () {
+      testUserId = Accounts.createUser({ username: "testUser", password: "pass" });
+    });
     beforeEach(function () {
+      Listings.remove({});
+    });
+    after(function () {
+      Meteor.users.remove({ _id: testUserId });
       Listings.remove({});
     });
 
     it('can createListing', function () {
       const addListing = Meteor.server.method_handlers['createListing'];
-
-      addListing.apply({}, ['babysitting', 'services']);
-
+      addListing.apply({ userId: testUserId }, [{ title: 'apple', description: '', tags: '#fruit', isNeed: false }]);
       assert.equal(Listings.find().count(), 1);
     });
   });
--- imports/api/publications/listings.tests.js
+++ imports/api/publications/listings.tests.js
@@ -1,21 +1,39 @@
 import assert from 'assert';
+import { Accounts } from 'meteor/accounts-base';
 import { PublicationCollector } from 'meteor/johanbrook:publication-collector'
 import Listings from '../collections/Listings.js'
 import '../publications/listings.js'
 
 describe('Publish listings', function () {
+  let testUserId;
+  before(function () {
+    testUserId = Accounts.createUser({ username: "testUser", password: "pass" });
+  });  
   beforeEach(function () {
     Listings.remove({})
     Listings.insert({
       title: 'Life, universe, everything?',
-      category: 'services'
+      category: 'services',
+      createdBy: testUserId,
+    })
+  })
+  after(function () {
+    Meteor.users.remove({ _id: testUserId });
+    Listings.remove({})
+  });
+  it('sends all listings to logged in user', function (done) {
+    assert.equal(Listings.find().count(), 1);
+    const collector = new PublicationCollector({ userId: testUserId });
+    collector.collect('listings', (collections) => {
+      assert.equal(collections.listings.length, 1);
+      done();
     })
   })
 
-  it('sends all listings', function (done) {
-    const collector = new PublicationCollector()
+  it('does not publish to not logged in user', function (done) {
+    const collector = new PublicationCollector();
     collector.collect('listings', (collections) => {
-      assert.equal(collections.listings.length, 1)
+      assert.equal(collections.listings, undefined);
       done()
     })
   })
