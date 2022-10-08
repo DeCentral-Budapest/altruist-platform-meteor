8f875a0f69e8f9cb04b2d58ca536636336162449
droka <daniel.roka@tutanota.com>
2022-10-09 01:25:08 +0200

    migration tidy up

--- server/migrations.js
+++ server/migrations.js
@@ -4,15 +4,15 @@ import { Migrations } from 'meteor/percolate:migrations';
 //import Deals from '/imports/api/collections/Deals.js';
 
 Migrations.add({
-    version: 1,
-    name: 'Renamed transactions to deals',
-    up() {
-        const Transactions = new Mongo.Collection('transactions');
-        Transactions.rawCollection().rename('deals');
-    },
-  });
+  version: 1,
+  name: 'Renamed transactions to deals',
+  up() {
+    const Transactions = new Mongo.Collection('transactions');
+    Transactions.rawCollection().rename('deals');
+  },
+});
 
-  Meteor.startup(() => {
-    Migrations.unlock();
-    Migrations.migrateTo('latest');
-  });
+Meteor.startup(() => {
+  Migrations.unlock();
+  Migrations.migrateTo('latest');
+});
