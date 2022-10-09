c06162f50f86a02d769428a96a4faf3b38a80dbd
eapo <e@eaposztrof.com>
2022-10-09 03:19:56 +0300

    Deals hits optimised and Button added to  with TODO

--- imports/api/collections/Deals.js
+++ imports/api/collections/Deals.js
@@ -14,31 +14,32 @@ Deals.statusObjects = {
     inquiry: {
         bgClass: 'bg-primary',
         faClass: 'fa-question', 
-        hint: 'Please discuss details with the other party and press accept once agreed in every detail.',
+        hint: 'Please discuss details with the other party and press <span class="bg-success text-light">Accept</span> once agreed in every detail.',
         todo: 'Accept',
     },
     canceled: {
         bgClass: 'bg-danger',
         faClass: 'fa-times', 
-        hint: 'This deal has been canceled',
+        hint: 'This <em>Deal</em> has been canceled',
+        todo: 'Leave review', // TODO
     },
     accepted: {
         bgClass: 'bg-success',
         faClass:  'fa-check',
-        hint: 'This deal has been accepted. Please deliver the goods or services and leave a review after the deal has succeded or failed',
+        hint: 'This <em>Deal</em> has been accepted. Please deliver the Goods or Services and leave a Review after the <em>Deal</em> has <span class="bg-success text-light">succeded</span> or <span class="bg-danger text-light">failed</a>',
         todo: 'Leave review',
     },
     disputed: {
         bgClass: 'bg-warning',
         faClass:  'fa-exclamation',
-        hint: 'This deal is currently under dispute',
+        hint: 'This <em>Deal</em> is currently under dispute',
+        todo: 'Accept', // TODO +Accept +Cancel
     },
     reviewed: {
-        bgClass: '',
-        faClass: '',
-        hint: 'This deal has been reviewed by both parties',
+        bgClass: 'bg-secondary',
+        faClass: 'fa-signature-lock',
+        hint: 'This <em>Deal</em> has been reviewed by both parties <span class="text-muted">and <em>Altruism Score</em> was redistributed</span>',
     },
 }
-
 
 export default Deals;
