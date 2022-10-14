999861eb1f6197631a85854d45650ff82f5ba019
eapo <e@eaposztrof.com>
2022-10-14 21:47:53 +0300

    roti.express Feedback in alert

--- imports/ui/components/Footr.vue
+++ imports/ui/components/Footr.vue
@@ -10,6 +10,17 @@
     </figure>
     <div class="nav container overflow-hidden mb-5" v-if="currentUser">
       <FooterNav/>
+      <div class="alert alert-success alert-dismissible fade show text-center fixed-bottom top-right m-5 pb-0 col-lg-4 col-md-6 col-sm-8" role="alert">
+        <h3 class="form-check-inline"><a href="https://roti.express/r/hmequl" target="_blank">Please give Us <br />some Feedback</a></h3>
+        <p class="form-check-inline">
+          using
+          <br />
+          <a href="https://roti.express/r/hmequl" target="_blank">
+            <img src="https://roti.express/img/roti-magnet.png" alt="roti.express" style="max-height:5em;" />
+          </a>
+        </p>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   </footer>
 </template>
