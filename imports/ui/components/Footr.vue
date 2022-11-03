237c77b45a749fcb27fcb67c4dbd5f54939136a1
eapo <e@eaposztrof.com>
2022-11-03 02:04:32 +0200

    Replace feedback with GitHub URLs in dismissable alert

--- imports/ui/components/Footr.vue
+++ imports/ui/components/Footr.vue
@@ -11,12 +11,29 @@
     <div class="nav container overflow-hidden mb-5" v-if="currentUser">
       <FooterNav/>
       <div class="alert alert-success alert-dismissible fade show text-center fixed-bottom top-right m-5 pb-0 col-lg-4 col-md-6 col-sm-8" role="alert">
-        <h3 class="form-check-inline"><a href="https://roti.express/r/hmequl" target="_blank">Please give Us <br />some Feedback</a></h3>
+        <ul>
+          <li>
+            <a href="https://github.com/alTRUEco/alTRUE.meteor" target="_blank">
+              <i class="fa-brands fa-github fa-fw"></i>
+              Source code
+            </a>
+          </li>
+          <li>
+            <a href="https://doc.alTRU.Eco/" target="_blank">
+              <i class="fa fa-pencil-square-o fa-fw"></i>
+              Documentation
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/alTRUEco/alTRUE.meteor/issues" target="_blank">
+              <i class="fa fa-dot-circle-o fa-fw"></i>
+              Issues/feature requests
+            </a>
+          </li>
+        </ul>
         <p class="form-check-inline">
-          using
-          <br />
-          <a href="https://roti.express/r/hmequl" target="_blank">
-            <img src="https://roti.express/img/roti-magnet.png" alt="roti.express" style="max-height:5em;" />
+          <a href="https://alTRU.Eco" target="_blank">
+            <img src="https://altru.eu.meteorapp.com/ufs/photoStore/rsoZAijC7qHYzxKPz/alTRUEco-logo-sq-tr-c.png" alt="roti.express" style="max-height:5em;" />
           </a>
         </p>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
